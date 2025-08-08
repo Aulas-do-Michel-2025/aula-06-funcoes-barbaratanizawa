708e2db49419a348e6cef61af74e9c5e413a5371
Bárbara Tanizawa <barbaratanizawa@gmail.com>
2025-08-08 17:43:39 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -1,11 +1,2 @@
-"""
-Exercício 3 - Calculando a média de uma lista
-
-Escreva uma funcao chamada `calcular_media` (sem acento) que recebe uma lista com numeros
-e retorne a média dos valores dela.
-
-Exemplo de uso:
->>> print(calcular_media([0, 100, 200]))
->>> 100
-"""
-
+def calcular_media(lista):
+    return sum(lista) / len(lista)
